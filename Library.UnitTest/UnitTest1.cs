54ee97d9d9163795bee0bca7702db68b8dd887cb
JC <kuba.czekiel@gmail.com>
2022-07-31 12:51:14 +0200

    HC-01

--- Library.UnitTest/UnitTest1.cs
+++ Library.UnitTest/UnitTest1.cs
@@ -6,37 +6,12 @@ namespace Library.UnitTest
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        [DataRow(1,0,-1)]
+        public void TestOczekiwanPracyW_HC_WORKS(int oczekiwania_z_HC)
         {
-            //Arrange
-            string[] Summaries = new[]
-            {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
+            bool isOnGoodLevel = oczekiwania_z_HC < 0;
 
-            var controller = new WeatherForecastController(null);
-
-            //Act
-            var weatherForNextWeek = controller.Get();
-
-            //Assert
-
-            Assert.NotNull(weatherForNextWeek);
-            Assert.True(weatherForNextWeek.Count() == 5);
-
-
-            for(int i = 0; i<5; i++)
-            {
-                WeatherForecast? weatherDay = weatherForNextWeek.ElementAt(i);
-                Assert.NotNull(weatherDay);
-                Assert.True(weatherDay.Date.Day == DateTime.Now.AddDays(i+1).Day);
-                Assert.True(weatherDay.TemperatureC >= -22);
-                Assert.True(weatherDay.TemperatureC < 55);
-                Assert.Contains(weatherDay.Summary, Summaries);
-            }
-
+            Assert.IsTrue(isOnGoodLevel);
         }
-
-        
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # LibraryAPI
 Basic REST API that allows to manage books in Database
+Written by basic HC member which is happy to stay there forever
